3799d4d177bc02d7383dc846ed8675a203a9b5af
connfiz <conndylanf@gmail.com>
2024-05-09 00:00:30 +0100

    host changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  server: { https: true },
+  server: { host: true, port: 80,},
   define: { 'process.env.BUNGIE_API_KEY': '"ac1948bd5fc0489fa439f828bbdf96fd"', BUNGIE_APP_INFO: { api_key: "ac1948bd5fc0489fa439f828bbdf96fd", client_id: "45938", client_secret: "9ZCMnmvyEJDHuKVYNHV4rTdZL-vRYWWcsmtVKgH-mME" }  },
   base: "./",
 });
